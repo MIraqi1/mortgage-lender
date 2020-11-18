9654d3c23e057869c8d8b3a065183d88d8375c73
mandah-a11y <mandakhgerel.ganbold1@t-mobile.com>
2020-11-18 10:28:46 -0800

    initial setup, able to check funds

--- src/main/java/Lender.java
+++ src/main/java/Lender.java
@@ -1,3 +1,12 @@
 final class Lender {
 
+    public Lender(int balance) {
+        this.balance = balance;
+    }
+
+    private int balance;
+
+    public int getBalance() {
+        return this.balance;
+    }
 }
--- src/test/java/LenderTest.java
+++ src/test/java/LenderTest.java
@@ -1,8 +1,15 @@
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 final class LenderTest {
 
+    @Test
+    void check_balance() {
+        Lender lender = new Lender(500);
+        Assertions.assertEquals(500,lender.getBalance());
+    }
 }
